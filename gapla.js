8b04e8968ea7032b0446250435601621677f44b3
Bishal Dey <deybishal2k15@gmail.com>
2019-03-26 15:50:20 +0530

    added rendomString generator in GaplaJS

--- gapla.js
+++ gapla.js
@@ -156,6 +156,15 @@ class GaplaJS {
             .then(res => resolve(res.json()))
         })
     }
+    randomString(len, an){
+        an = an&&an.toLowerCase();
+        var str="", i=0, min=an=="a"?10:0, max=an=="n"?10:62;
+        for(;i++<len;){
+          var r = Math.random()*(max-min)+min <<0;
+          str += String.fromCharCode(r+=r>9?r<36?55:61:48);
+        }
+        return str;
+    }
 }
 
 export const Gapla = new GaplaJS()
